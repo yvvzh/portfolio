1aa41f3e1fff132e9cd5ea60e250aaf87fcc5d50
yvvzh <135761301+yvvzh@users.noreply.github.com>
2024-01-18 16:26:40 +0100

    calculator done

---
+++ projects/calculator/calculator.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function percentage() {
+    display.value = display.value/100;
+}
+
+function sign() {
+    display.value = -display.value;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
